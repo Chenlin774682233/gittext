ff3e5e641db5618fe36d060abae616aef084c691
ChenLin <774682233@qq.com>
2020-04-01 16:58:16 +0800

    提交测试文件

---
+++ git-test-project/src/com/git/first.java
@@ -0,0 +1,12 @@
+package com.git;
+
+import static java.lang.System.*;
+
+public class first {
+    public static void main(String[] args) {
+        out.println("Hello,Git!");
+        out.println("Hello,Git!");
+        out.println("Hello,Git!");
+        out.println("Hello,Git!");
+    }
+}
